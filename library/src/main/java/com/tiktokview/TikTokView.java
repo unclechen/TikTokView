a19693f1ff5b940267fa8a8fd953ea3a45a5c92a
unclechen <xh-chen@163.com>
2015-12-04 14:13:38 +0800

    measure the view size

--- library/src/main/java/com/tiktokview/TikTokView.java
+++ library/src/main/java/com/tiktokview/TikTokView.java
@@ -1,5 +1,7 @@
 package com.tiktokview;
 
+import com.tiktokview.utils.PxUtils;
+
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
@@ -7,9 +9,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
-
-import com.tiktokview.utils.PxUtils;
 
 /**
  * TikTokView is a time TextView.
@@ -20,7 +21,6 @@ public class TikTokView extends View {
     private Context mContext;
     private Paint mPaint;
     private Rect mBounds;
-    private String mTextFormat = "%d";
     private TikTokMode mTikTokMode = TikTokMode.DECREMENT;
     private int mBackgroundColor;
     private float mPadding;
@@ -28,7 +28,8 @@ public class TikTokView extends View {
     private int mTextColor;
     private int mTikTokTintColor;
     private long mCurrentTime;
-    private long mTotalTime;
+    private long mStartTime;
+    private long mEndTime;
 
     public enum TikTokMode {
         INCREMENT,
@@ -50,7 +51,6 @@ public class TikTokView extends View {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.TikTokView);
         mTextColor = typedArray.getColor(R.styleable.TikTokView_text_color, Color.BLACK);
         mBackgroundColor = typedArray.getColor(R.styleable.TikTokView_background_color, Color.TRANSPARENT);
-        mTikTokTintColor = typedArray.getColor(R.styleable.TikTokView_tik_tok_color, Color.BLACK);
         mTextSize = typedArray.getLayoutDimension(R.styleable.TikTokView_text_size, PxUtils.sp2px(mContext, 30));
         typedArray.recycle();
     }
@@ -96,14 +96,6 @@ public class TikTokView extends View {
         invalidate();
     }
 
-    public String getTextFormat() {
-        return mTextFormat;
-    }
-
-    public void setTextFormat(String textFormat) {
-        mTextFormat = textFormat;
-    }
-
     public float getTextSize() {
         return mTextSize;
     }
@@ -130,12 +122,20 @@ public class TikTokView extends View {
         invalidate();
     }
 
-    public long getTotalTime() {
-        return mTotalTime;
+    public long getStartTime() {
+        return mStartTime;
     }
 
-    public void setTotalTime(long totalTime) {
-        mTotalTime = totalTime;
+    public void setStartTime(long startTime) {
+        mStartTime = startTime;
+    }
+
+    public long getEndTime() {
+        return mEndTime;
+    }
+
+    public void setEndTime(long endTime) {
+        mEndTime = endTime;
     }
 
     public void start() {
@@ -154,18 +154,81 @@ public class TikTokView extends View {
 
     }
 
-    // 深入了解一下canvas都可以做什么
     @Override
     protected void onDraw(Canvas canvas) {
+        Log.d("TikTokView", "onDraw");
         super.onDraw(canvas);
+        // draw background
         mPaint.setColor(mBackgroundColor);
         canvas.drawRect(0, 0, getWidth(), getHeight(), mPaint);
+        // draw text
         mPaint.setColor(mTextColor);
         mPaint.setTextSize(mTextSize);
-        mPaint.getTextBounds(mTextFormat, 0, mTextFormat.length(), mBounds);
+        String text;
+        if (TikTokMode.DECREMENT == mTikTokMode) {
+            text = mEndTime + "";
+        } else {
+            text = mStartTime + "";
+        }
+        mPaint.getTextBounds(text, 0, text.length(), mBounds);
         float textWidth = mBounds.width();
         float textHeight = mBounds.height();
-        canvas.drawText(mTextFormat, getWidth() / 2 - textWidth / 2, getHeight() / 2 + textHeight / 2, mPaint);
+        canvas.drawText(text, getWidth() / 2 - textWidth / 2, getHeight() / 2 + textHeight / 2, mPaint);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = getMeasuredWidth(); // MeasureSpec.getSize(measureSpec);
+        int height = getMeasuredHeight();
+        Log.d("TikTok", "onMeasure width = " + width);
+        Log.d("TikTok", "onMeasure height = " + height);
+        int widthWithoutPadding = width - getPaddingLeft() - getPaddingRight();
+        int heightWithoutPadding = height - getPaddingTop() - getPaddingBottom();
+        Log.d("TT", "padding left = " + getPaddingLeft());
+        Log.d("TikTok", "getDesiredWidth = " + getDesiredWidth());
+        Log.d("TikTok", "getDesiredHeight = " + getDesiredHeight());
+        int specMode = MeasureSpec.getMode(widthMeasureSpec);
+        switch (specMode) {
+            case MeasureSpec.UNSPECIFIED:
+                width = (int) getDesiredWidth();
+                height = (int) getDesiredHeight();
+                break;
+            case MeasureSpec.AT_MOST:
+                width = Math.min((int) getDesiredWidth(), width);
+                height = Math.min((int) getDesiredHeight(), height);
+                break;
+            case MeasureSpec.EXACTLY:
+                break;
+        }
+
+        setMeasuredDimension(width, height);
+    }
+
+    private float getDesiredWidth() {
+        String text;
+        if (TikTokMode.DECREMENT == mTikTokMode) {
+            text = mEndTime + "";
+        } else {
+            text = mStartTime + "";
+        }
+        mPaint.getTextBounds(text, 0, text.length(), mBounds);
+        float textWidth = mBounds.width();
+        float textHeight = mBounds.height();
+        return textWidth;
+    }
+
+    private float getDesiredHeight() {
+        String text;
+        if (TikTokMode.DECREMENT == mTikTokMode) {
+            text = mEndTime + "";
+        } else {
+            text = mStartTime + "";
+        }
+        mPaint.getTextBounds(text, 0, text.length(), mBounds);
+        float textWidth = mBounds.width();
+        float textHeight = mBounds.height();
+        return textHeight;
     }
 
 }
